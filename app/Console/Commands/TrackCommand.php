ba5d08dd7e3078bcae1ab6f07f02d0cbbb7fa79f
Jeffrey Way <jeffrey@jeffrey-way.com>
2020-05-27 14:09:50 -0400

    Episode 14 Complete

--- app/Console/Commands/TrackCommand.php
+++ app/Console/Commands/TrackCommand.php
@@ -7,29 +7,39 @@ use Illuminate\Console\Command;
 
 class TrackCommand extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
     protected $signature = 'track';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
     protected $description = 'Track all product stock.';
 
-    /**
-     * Execute the console command.
-     *
-     * @return mixed
-     */
     public function handle()
     {
-        Product::all()->each->track();
+        Product::all()
+            ->tap(fn($products) => $this->output->progressStart($products->count()))
+            ->each(function ($product) {
+                $product->track();
 
-        $this->info('All done!');
+                $this->output->progressAdvance();
+            });
+
+        $this->showResults();
+    }
+
+    protected function showResults(): void
+    {
+        $this->output->progressFinish();
+
+        $data = Product::query()
+            ->leftJoin('stock', 'stock.product_id', '=', 'products.id')
+            ->get($this->keys());
+
+        $this->table(
+            array_map('ucwords', $this->keys()),
+            $data
+        );
+    }
+
+    protected function keys(): array
+    {
+        return ['name', 'price', 'url', 'in_stock'];
     }
 }
--- tests/Feature/TrackCommandTest.php
+++ tests/Feature/TrackCommandTest.php
@@ -5,7 +5,6 @@ namespace Tests\Feature;
 use App\Product;
 use Tests\TestCase;
 use RetailerWithProductSeeder;
-use Illuminate\Support\Facades\Notification;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class TrackCommandTest extends TestCase
@@ -15,16 +14,13 @@ class TrackCommandTest extends TestCase
     /** @test */
     function it_tracks_product_stock()
     {
-        Notification::fake();
-
         $this->seed(RetailerWithProductSeeder::class);
 
         $this->assertFalse(Product::first()->inStock());
 
         $this->mockClientRequest();
 
-        $this->artisan('track')
-            ->expectsOutput('All done!');
+        $this->artisan('track');
 
         $this->assertTrue(Product::first()->inStock());
     }
